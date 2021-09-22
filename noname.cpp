e78343e46d7acf86703567a7cb0e33d1fc3c4762
aierdululuomandike <1592479402@qq.com>
2021-09-22 16:23:03 +0800

    forth try for comments

--- noname.cpp
+++ noname.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <queue>
 #include <ctype.h>
+#include <vector>
 using namespace std; 
 
 const string keyWords[32] = {
@@ -15,14 +16,14 @@ const string keyWords[32] = {
 	"typedef", "union", "unsigned", "void", "volatile", "while"
 };
 
-void isKeyWords(string word, int &keyWordNum)//简单的单词匹配 
+void isKeyWords(string word, int &keyWordNum ,vector<string> &words)//简单的单词匹配 
 {
 	for(int i = 0; i < 32; i++)
 	{
 		if(keyWords[i] == word)
 		{
 			keyWordNum++;
-			cout<<word<<endl;
+			words.push_back(word);
 			break;
 		}
 	}
@@ -42,12 +43,12 @@ string getRightString(string str)//处理字符串中的特殊情况：// “” /**/
 				isit = 1;
 				continue;
 			}
-			else if(str[i] == '/' && str[i + 1] == '*')
-			{
-				isit = 2;
-				i++;
-				continue;
-			}
+//			else if(str[i] == '/' && str[i + 1] == '*')
+//			{
+//				isit = 2;
+//				i++;
+//				continue;
+//			}
 			else if(str[i] == str[i + 1] && str[i] == '/')
 			{
 				break;
@@ -63,29 +64,29 @@ string getRightString(string str)//处理字符串中的特殊情况：// “” /**/
 			{
 				isit = 0;
 			}
-			else if(isit == 2)
-			{
-				if(str[i] == '*' && str[i + 1] == '/')
-				{
-					isit = 0;
-					i++;
-				}
-			}
+//			else if(isit == 2)
+//			{
+//				if(str[i] == '*' && str[i + 1] == '/')
+//				{
+//					isit = 0;
+//					i++;
+//				}
+//			}
 		}
 	
 	}
 	return ans;
 }
 
-void keyWordNum(ifstream &infile, int &ans)//计算单词数量也就是第一环节 
+void keyWordNum(ifstream &infile, int &ans, vector<string> &words)//第一环节:计算数量 
 {
 	string str;
 	ans = 0;
-	
 	int len = 0;
-	
+		
 	if(infile.is_open())
 	{
+		bool isLine = true;
 		while(getline(infile, str))//得到一行 
 		{	
 			str = getRightString(str);	
@@ -94,37 +95,60 @@ void keyWordNum(ifstream &infile, int &ans)//计算单词数量也就是第一环节
 			int wordEnd = 0;
 			bool isLetter = false;
 			string word;
+			str = getRightString(str);
 			for(int i = 0; i < len; i++)
 			{
-				if(str[i] <= 'z' && str[i] >= 'a')
+				if(isLine)
 				{
-					if(!isLetter)
+					if(str[i] == '/' && str[i + 1] == '*')
 					{
-						isLetter = true;
-						wordBegin = i;
+						isLine = false;
+						i++;
+						continue;
 					}
-					else 
+					if(str[i] <= 'z' && str[i] >= 'a')
 					{
-						if(i + 1 == len)
+						if(!isLetter)
 						{
-							wordEnd = i + 1; 
-							word = str.substr(wordBegin, wordEnd - wordBegin);
-							isKeyWords(word, ans);
+							isLetter = true;
+							wordBegin = i;
+						}
+						else 
+						{
+							if(i + 1 == len)
+							{
+								wordEnd = i + 1; 
+								word = str.substr(wordBegin, wordEnd - wordBegin);
+								isKeyWords(word, ans, words);
+							}
 						}
 					}
+					else if(isLetter)
+					{
+						isLetter = false;
+						wordEnd = i;
+						word = str.substr(wordBegin, wordEnd - wordBegin);
+						isKeyWords(word, ans, words);
+					}
 				}
-				else if(isLetter)
+				else
 				{
-					isLetter = false;
-					wordEnd = i;
-					word = str.substr(wordBegin, wordEnd - wordBegin);
-					isKeyWords(word, ans);
-				}
+					if(str[i] == '*' && str[i + 1] == '/')
+					{
+						isLine = true;
+						i++;
+					}	
+				}	
 			}	
 		}
 	}
 }
 
+void switchCaseNum(vector<string> A)
+{
+	
+}
+ 
 int main(){
 	string location;
 	int rank;
@@ -141,7 +165,12 @@ int main(){
 		cerr<<"error:unable to open input file"<<endl;
 	}
 	int ans;
-	keyWordNum(infile, ans);
+	vector<string> words;
+	keyWordNum(infile, ans,words);
+	for(auto &x:words)
+	{
+		cout<<x<<endl;
+	}
 	cout<<"total num: "<<ans<<endl;
 	return 0;
 }
